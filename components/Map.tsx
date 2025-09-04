9f487795b97903112dca4f02445cb6303085a9ab
Serdar Geldyev <serdarabad@gmail.com>
2025-09-04 11:18:59 +0300

    dynamic imports

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import dynamic from "next/dynamic";
+
+import { ComponentType } from "react";
 import MarkerClusterGroup from "react-leaflet-markercluster";
 import styles from "./Map.module.scss";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
@@ -8,28 +11,29 @@ import { DivIcon, divIcon, Icon, LatLngExpression, point } from "leaflet";
 const position: LatLngExpression = [46.8523, -121.7605];
 const leaf = "./leaflet/mount.jpg";
 
-const Map: React.FC = () => {
-  const markers: { coords: LatLngExpression; text: string }[] = [
-    {
-      coords: position,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident neque modi aut nostrum voluptatibus, quos illum aperiam reiciendis est nam pariatur?",
-    },
-    {
-      coords: [position[0], position[1] + 0.01],
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident neque modi aut nostrum voluptatibus, quos illum aperiam reiciendis est nam pariatur?",
-    },
-    {
-      coords: [position[0] + 0.005, position[1] - 0.01],
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident neque modi aut nostrum voluptatibus, quos illum aperiam reiciendis est nam pariatur?",
-    },
-  ];
-  const myIcon = new Icon({
-    iconSize: [38, 38],
-    iconUrl: "./leaflet/marker-icon.png",
-    shadowUrl: "./leaflet/marker-shadow.png",
-    shadowSize: [38, 38],
-  });
+const markers: { coords: LatLngExpression; text: string }[] = [
+  {
+    coords: position,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident neque modi aut nostrum voluptatibus, quos illum aperiam reiciendis est nam pariatur?",
+  },
+  {
+    coords: [position[0], position[1] + 0.01],
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident neque modi aut nostrum voluptatibus, quos illum aperiam reiciendis est nam pariatur?",
+  },
+  {
+    coords: [position[0] + 0.005, position[1] - 0.01],
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident neque modi aut nostrum voluptatibus, quos illum aperiam reiciendis est nam pariatur?",
+  },
+];
 
+const myIcon = new Icon({
+  iconSize: [38, 38],
+  iconUrl: "./leaflet/marker-icon.png",
+  shadowUrl: "./leaflet/marker-shadow.png",
+  shadowSize: [38, 38],
+});
+
+function Map() {
   const makeIcon = (cluster: { getChildCount: () => number }): DivIcon => {
     return divIcon({
       html: `<div class=${styles["cluster-icon"]}>${cluster.getChildCount()}</div>`,
@@ -63,5 +67,6 @@ const Map: React.FC = () => {
       </MarkerClusterGroup>
     </MapContainer>
   );
-};
+}
+
 export default Map;
