c210af69a367f69c0b5f45ac40d17a4b7b4a1327
Serdar Geldyev <serdarabad@gmail.com>
2025-10-06 23:08:35 +0300

    feat(ui): units change in widget

--- components/SpeedDial/SpeedDial.tsx
+++ components/SpeedDial/SpeedDial.tsx
@@ -11,13 +11,56 @@ import ThermostatIcon from "@mui/icons-material/Thermostat";
 import SettingsInputSvideoIcon from "@mui/icons-material/SettingsInputSvideo";
 import style from "./SpeedDial.module.scss";
 import Tooltip from "@mui/material/Tooltip";
-const actions = [
-  { icon: <SpeedIcon />, name: "Speed" },
-  { icon: <LandscapeIcon />, name: "Height" },
-  { icon: <ThermostatIcon />, name: "Temperature" },
-];
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+
+interface Actions {
+  icon: React.JSX.Element;
+  name: string;
+  value: string[];
+  currentValue: number;
+}
 
 export default function UnitsSpeedDial() {
+  const [speedUnits, setSpeedUnits] = React.useState<Actions>({
+    icon: <SpeedIcon />,
+    name: "Speed",
+    value: ["m/s", "mph", "km/h"],
+    currentValue: 0,
+  });
+  const [heightUnits, setHeightUnits] = React.useState<Actions>({
+    icon: <LandscapeIcon />,
+    name: "Height",
+    value: ["m", "ft"],
+    currentValue: 0,
+  });
+  const [tempUnits, setTempUnits] = React.useState<Actions>({
+    icon: <ThermostatIcon />,
+    name: "Temperature",
+    value: ["C", "F"],
+    currentValue: 0,
+  });
+
+  const actions = [speedUnits, heightUnits, tempUnits];
+
+  const handleActionChange = (actionName: string) => {
+    if (actionName === "Speed") {
+      setSpeedUnits({
+        ...speedUnits,
+        currentValue: (speedUnits.currentValue + 1) % 3,
+      });
+    } else if (actionName === "Height") {
+      setHeightUnits({
+        ...heightUnits,
+        currentValue: (heightUnits.currentValue + 1) % 2,
+      });
+    } else if (actionName === "Temperature") {
+      setTempUnits({
+        ...tempUnits,
+        currentValue: (tempUnits.currentValue + 1) % 2,
+      });
+    }
+  };
+
   return (
     <Box sx={{ height: "4rem", transform: "translateZ(0px)", flexGrow: 1 }}>
       <SpeedDial
@@ -38,10 +81,36 @@ export default function UnitsSpeedDial() {
           <SpeedDialAction
             key={action.name}
             icon={action.icon}
-            onClick={() => console.log(action.name)}
+            onClick={() => handleActionChange(action.name)}
             slotProps={{
               tooltip: {
-                title: action.name,
+                title: (
+                  <>
+                    <ToggleButtonGroup
+                      size="small"
+                      value={action.value[action.currentValue]}
+                      exclusive
+                      onChange={() => {}}
+                      aria-label={action.name}
+                    >
+                      {action.value.map((value) => (
+                        <ToggleButton
+                          color="warning"
+                          sx={{
+                            color: "white",
+                            fontWeight: "bold",
+                            borderColor: "white",
+                            cursor: "default",
+                          }}
+                          key={value}
+                          value={value}
+                        >
+                          {value}
+                        </ToggleButton>
+                      ))}
+                    </ToggleButtonGroup>
+                  </>
+                ),
               },
             }}
           />
